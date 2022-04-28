07d0a9ba7475e15d164cf78ced4952691f89e233
João Sino <74789353+Joaosino@users.noreply.github.com>
2022-04-28 11:59:15 -0300

    Add files via upload

---
+++ Aps Estrutura de dados/menuAps.py
@@ -0,0 +1,22 @@
+def escolheJogo() :
+  print("""
+
+    Olá, eu sou o navegador de jogos! aqui você poderá escolher entre 4 opções sendo elas Xadrez, Sudoku, adivinhe o número e outro que ainda não existe
+
+    escolha um jogo para começar ou aperte o 0 para terminar a aplicação!
+
+    (1)Xadrez
+    (2)Sudoku
+    (3)Adivinhe o número
+    (4)Outro
+    (0)Encerrar a aplicação
+      
+      """)
+  escolha = int(input("escolha: "))
+  print(escolha)
+
+escolheJogo()
+
+
+
+
