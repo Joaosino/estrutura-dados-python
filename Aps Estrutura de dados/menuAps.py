7e83a39d44cc8b2b2adc253535624c04028010c1
João Sino <74789353+Joaosino@users.noreply.github.com>
2022-05-03 16:57:55 -0300

    Add files via upload

---
+++ Aps Estrutura de dados/menuAps.py
@@ -0,0 +1,36 @@
+def menuDeJogos(escolha):
+  if escolha == 0:
+    print('Obrigado por ter jogado nosso jogo!')
+    quit() 
+  if escolha == 1:
+    import blackJack
+    blackJack.jogo()
+  if escolha == 2:
+    print('algo de errado não está certo')
+  if escolha == 3:
+    import madlib
+    madlib.madlib()
+  if escolha == 4:
+    return 
+
+
+def menuInicial() :  
+  print("""
+
+    Olá, eu sou o navegador de jogos! aqui você poderá escolher entre 4 opções sendo elas BlackJack, Adivinhe o Número, MadLibs e outro que ainda não existe
+
+    escolha um jogo para começar ou aperte o 0 para terminar a aplicação!
+
+    (1)BlackJack
+    (2)Adivinhe o número
+    (3)MadLibs
+    (4)Outro
+    (0)Encerrar a aplicação
+      
+      """)
+  escolha = int(input("escolha: "))
+  menuDeJogos(escolha)
+
+
+
+menuInicial()
